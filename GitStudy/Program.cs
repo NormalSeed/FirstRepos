0130d9e3d640457119af45b008e25c2f59793385
NormalSeed <dlrjsghk11@naver.com>
2025-03-20 12:42:55 +0900

    Feat new task
    새로운 기능 추가

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -10,10 +10,7 @@ namespace GitStudy
     {
         static void Main(string[] args)
         {
-            Console.Write("이름을 입력해 주세요 : ");
-            string input = Console.ReadLine();
-
-            Console.WriteLine("입력하신 이름은 {0}입니다.", input);
+            Console.WriteLine("새로운 기능 추가!");
         }
     }
 }
