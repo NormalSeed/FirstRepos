5c457cc7de35fc0989ed456bdb76b51e6b463232
NormalSeed <dlrjsghk11@naver.com>
2025-03-20 12:43:37 +0900

    Feat add new task
    추가 기능 추가

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -11,6 +11,7 @@ namespace GitStudy
         static void Main(string[] args)
         {
             Console.WriteLine("새로운 기능 추가!");
+            Console.WriteLine("추가 기능 추가!");
         }
     }
 }
